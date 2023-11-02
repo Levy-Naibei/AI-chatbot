b83f2d986d20f1f88ea6d53dcd8b2dfc4de5f0d9
Levy-Naibei <levynaibei2@gmail.com>
2023-11-03 00:06:27 +0300

    added morgan middlware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,18 @@
 import express from "express"
 import { config } from "dotenv";
+import morgan from "morgan"
+import appRouter from "./routes/index.js";
 
 // load .env contents into process.env
 config()
 
 const app = express();
+
+// in dev only
+app.use(morgan("dev"))
+
+// register routes
+app.use("/api/v1", appRouter);
 
 // middlewares
 app.use(express.json())
