0e07fedcd5c518e3dd0c177fc40b5d8690e70a5a
Levy-Naibei <levynaibei2@gmail.com>
2023-11-06 15:08:22 +0300

    added user signup controller

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,12 +1,26 @@
-// fetch users from db
-export const getUsers = () => {
-    
-}
+import { Request, Response, NextFunction } from "express"
+import User from '../models/user.js'
+import { compare, hash } from "bcrypt";
 
-export const getUser = (id: string) => {
+// register user controller
+export const signup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction) => {
+    try {
+        const { email, name, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = await User.findOne({ email: email });
+        if (user) {
+            return res.status(401).send("User already exist!")
+        } else {
+            const newUser = new User({ email, name, password: hashedPassword });
+            await newUser.save()
+            return res.status(201).json({ message: "Register sucess!", id: newUser._id.toString() })
+        }
 
-}
-
-export const deleteUser = (id: string) => {
-
+    } catch (error) {
+        console.log(error?.message);
+        return res.status(200).json({ message: "ERROR", cause: error?.message })
+    }
 }
