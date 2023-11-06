48726d2c7be98745144556f3bbaadd4806319415
Levy-Naibei <levynaibei2@gmail.com>
2023-11-06 15:11:38 +0300

    added signup user route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { getUsers, getUser, deleteUser } from "../controllers/userControllers.js";
+import { signup } from "../controllers/userControllers.js";
 
 const userRoutes = Router();
 
-userRoutes.get('/', getUsers);
-userRoutes.get('/id', getUser);
-userRoutes.delete('/id', deleteUser);  // must have admin permission
+userRoutes.post("/signup", signup) // base_url/api/v1/user/signup
 
 export default userRoutes;
