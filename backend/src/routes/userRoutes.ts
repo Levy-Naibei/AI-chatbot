ceae20c1883ab49e8ccbd1e50f0e03e58db74b4c
Levy-Naibei <levynaibei2@gmail.com>
2023-11-06 15:16:36 +0300

    added user login route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signup } from "../controllers/userControllers.js";
+import { signup, login } from "../controllers/userControllers.js";
 
 const userRoutes = Router();
 
 userRoutes.post("/signup", signup) // base_url/api/v1/user/signup
+userRoutes.post("/login", login) // base_url/api/v1/user/login
 
 export default userRoutes;
