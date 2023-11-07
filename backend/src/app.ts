7bb0397cec85f7e87ad149be77926723749236b7
Levy-Naibei <levynaibei2@gmail.com>
2023-11-07 18:26:53 +0300

    added cookie-parser module

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,22 @@
 import express from "express"
 import { config } from "dotenv";
 import morgan from "morgan"
+import cookieParser from "cookie-parser";
 import appRouter from "./routes/index.js";
 
 // load .env contents into process.env
-config()
+config();
 
 const app = express();
 
 // middlewares
 // this middleware should be on first 
 // line after express app initialization
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // in dev only
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 // register routes
 app.use("/api/v1", appRouter);
